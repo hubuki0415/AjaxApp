b80e3bd2a03cb71aa5c24ee12be57fcd66701482
hubuki0415 <mangan24@icloud.com>
2020-08-05 18:13:37 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,15 @@ class PostsController < ApplicationController
 
   
 
+  
   def create
     Post.create(content: params[:content])
+
     redirect_to action: :index
   end
 
   def checked
+    
     post = Post.find(params[:id])
     if post.checked then
       post.update(checked: false)
